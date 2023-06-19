fecf9ea722fc6703c17454be10895474872d2489
bekloid <bekloid@gmail.com>
2023-06-19 16:25:43 +0300

    create a test case

--- tests/test_check_swag.py
+++ tests/test_check_swag.py
@@ -1,1 +1,10 @@
 from pages.base_page import BasePage
+
+
+def test_link(browser):
+
+    demo_page = BasePage(browser)
+    demo_page.visit()
+    demo_page.find_name_field()
+    assert demo_page.find_pass_field()
+    assert demo_page.exist_icon()
